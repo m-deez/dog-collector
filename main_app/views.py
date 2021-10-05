ddfdca77c713faeddc4f58053fe562080cdd8abf
Matt DeZarn <matt.dezarn@gmail.com>
2021-10-04 22:34:06 -0700

    made dog model, and mighrated to the data base.

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,24 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
+
+class Dog:
+    def __init__(self, name, breed, image, bio):
+        self.name = name
+        self.breed = breed
+        self.image = image
+        self.bio = bio
+
+dogs = [
+    Dog("Lilly", "Beagle", "https://i.imgur.com/EuYvkb3.jpeg", "Lilly is the bestest gurl ever."),
+    Dog("Lilly2", "Also Beagle", "https://i.imgur.com/NgFZl5a.jpeg", "Lilly2 is a test."),
+    Dog("Lilly3", "Weiner doggo", "https://i.imgur.com/79RWIBH.jpeg", "Lilly3 is a doofus.")
+]
+
+class DogList(TemplateView):
+    template_name = 'dog_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["dogs"] = dogs
+        return context
